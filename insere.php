fd35cb68f47af75626ebf507e358036bc5de634a
Ariadiny Soares Cano <ariadiny.soares17@gmail.com>
2026-05-13 10:25:59 -0300

    Add files via upload

--- insere.php
+++ insere.php
@@ -1,13 +1,13 @@
 <?php
 include 'conecta.php';
+include 'consulta2.php';
 
 $numero = $_POST['campo2'];
 $cor = $_POST['campo1'];
-$resposta = array();
 
 if($conn->query("INSERT INTO camiseta VALUES (NULL, '".$cor."', '".$numero."')")){
-    $resposta = 'Camisa registrada com sucesso!';
-}else {
+    $resposta = tabela();
+}else { 
     $resposta = 'Nao foi possivel registrar';
 };
 echo $resposta;                                                                                   
